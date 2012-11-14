df436e8d1451141b6d775ca113ddb087f0af89c9
hansau22 <hansau22@tormail.org>
2012-11-14 16:29:54 +0100

    Development

--- latex-table.py
+++ latex-table.py
@@ -12,17 +12,56 @@ def getResult(value):
 class table:
 	def __init__(rows):
 		self.rows = rows
+		self.secondRow = []
 		self.firstRow = []
+		
+		self.start = "\\begin{tabular}"
+		self.end = "\\end{tabular}"
 		
 	def addValue(row, value):
 		if (row < 0 ) | (row > self.rows):
 			self.firstRow[row] = value
-		else if (row == self.rows:
+		elif (row == self.rows):
 			self.firstRow.append(value)
+			self.rows += 1
 			
 	def fillTable():
-		for( i in range(0, self.rows):
-			
+		self.secondRow = []
+		for i in range(0, self.rows):
+			self.secondRow.append(getResult(self.firstRow[i]))
+		
+	def __exportTable(filepath):
+		f = open(filepath, 'r')
+		f.clear()
+		
+		f.write(start)
+		
+		formatString = "{"
+		for i in range(0, self.rows):
+			formatString.append("|c|")
+		formatString.append("}")
+		f.write(formatString)
+		del formatString
+		
+		firstString = ""
+		for i in range(0, self.rows):
+			firstString.append(self.firstRow[i])
+			firstString.append("&")
+		firstString.append("\\\\")
+		f.write(firstString)
+		del firstString
+		
+		secondString = ""
+		for i in range(0, self.rows):
+			secondString.append(self.secondRow[i])
+			secondString.append("&")
+		secondString.append("\\\\")
+		f.write(secondString)
+		del secondString
+		
+		f.write(self.end)
+		
+				
 
 def main():
 	
